7027ac249d8342ecd2248fa391869e01a6819dd8
Juan Gerardo <jmendez1025@cue.edu.co>
2024-08-15 11:15:08 -0500

    initial commit

---
+++ src/test/java/com/emazon/msvc/stock/msvcstock/MsvcStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.msvc.stock.msvcstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
