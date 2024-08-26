6ac5e25d98801df4a304aa19d49b5a60b8140a89
Juan Gerardo <jmendez1025@cue.edu.co>
2024-08-26 16:00:07 -0500

    docs: article controller documentation implemented

--- src/main/java/com/emazon/msvc/stock/msvcstock/infrastructure/adapters/in/controllers/ArticleController.java
+++ src/main/java/com/emazon/msvc/stock/msvcstock/infrastructure/adapters/in/controllers/ArticleController.java
@@ -4,6 +4,10 @@ package com.emazon.msvc.stock.msvcstock.infrastructure.adapters.in.controllers;
 import com.emazon.msvc.stock.msvcstock.application.dtos.article.ArticleDto;
 import com.emazon.msvc.stock.msvcstock.application.dtos.article.CreateArticleDto;
 import com.emazon.msvc.stock.msvcstock.application.services.ArticleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/articles")
 @AllArgsConstructor
+@Tag(name = "Article Controller", description = "Article Management")
 public class ArticleController {
   private final ArticleService articleService;
-
+  @Operation(
+          summary = "Create an article",
+          description = "Create an article in the stock"
+  )
+  @ApiResponses(
+          value = {
+                  @ApiResponse(
+                          responseCode = "201",
+                          description = "Article created successfully"
+                  ),
+                  @ApiResponse(
+                          responseCode = "400",
+                          description = "Bad request, invalid data provided"
+                  )
+          }
+  )
   @PostMapping("/create")
   public ResponseEntity<ArticleDto> createArticle(@RequestBody CreateArticleDto articleDto){
     return new ResponseEntity<>(
