9ebcc9a573400c13eea97f41c8bad9a2c14786a9
Juan Gerardo <jmendez1025@cue.edu.co>
2024-08-27 15:34:41 -0500

    feat: get articles paginated and sorted by name, or brand, or category implemented.

--- src/main/java/com/emazon/msvc/stock/msvcstock/domain/factories/SortingValidationFactory.java
+++ src/main/java/com/emazon/msvc/stock/msvcstock/domain/factories/SortingValidationFactory.java
@@ -1,6 +1,7 @@
 package com.emazon.msvc.stock.msvcstock.domain.factories;
 
 import com.emazon.msvc.stock.msvcstock.domain.validations.SortingValidation;
+import com.emazon.msvc.stock.msvcstock.domain.validations.imp.ArticleSortingValidation;
 import com.emazon.msvc.stock.msvcstock.domain.validations.imp.BrandSortingValidation;
 import com.emazon.msvc.stock.msvcstock.domain.validations.imp.CategorySortingValidation;
 
@@ -12,6 +13,7 @@ public class SortingValidationFactory {
     return switch (domainClass.getSimpleName()) {
       case "Category" -> new CategorySortingValidation();
       case "Brand" -> new BrandSortingValidation();
+      case "Article" -> new ArticleSortingValidation();
       default -> throw new IllegalArgumentException("Invalid domain class");
     };
   }
--- src/main/java/com/emazon/msvc/stock/msvcstock/domain/ports/in/usecases/article/RetrieveArticleUseCase.java
+++ src/main/java/com/emazon/msvc/stock/msvcstock/domain/ports/in/usecases/article/RetrieveArticleUseCase.java
@@ -1,11 +1,11 @@
-package com.emazon.msvc.stock.msvcstock.domain.ports.out.repositories;
+package com.emazon.msvc.stock.msvcstock.domain.ports.in.usecases.article;
 
 import com.emazon.msvc.stock.msvcstock.domain.models.Article;
 import com.emazon.msvc.stock.msvcstock.domain.models.Paginated;
 import com.emazon.msvc.stock.msvcstock.domain.models.Pagination;
 import com.emazon.msvc.stock.msvcstock.domain.models.Sorting;
 
-public interface ArticleRepository {
-  Article save(Article article);
+
+public interface RetrieveArticleUseCase {
   Paginated<Article> retrieveArticles(Pagination pagination, Sorting sorting);
 }
